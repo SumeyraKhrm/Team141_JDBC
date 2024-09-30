d38a0cabfaa649b63728aa57207b2557f2310b6e
SumeyraKhrm <sumeyra.khrm@outlook.com>
2024-09-30 18:03:36 +0200

    v4

--- src/test/java/manage/QueryManage.java
+++ src/test/java/manage/QueryManage.java
@@ -3,15 +3,17 @@ package manage;
 public class QueryManage {
 
     private String query02 ="SELECT name FROM cron_schedules limit 2";
-
     private String query03 ="select * from users where email = 'test@gmail.com'";
-
     // private ı kullanabilmek için başka bir class da getter olusturuyoruz.
-
     private String updateQuery01="update users set mobile = 05763452555 where username like '%r_'";
-
-
-
+    private String preparedQuery01 = "update users set mobile = ? where username like ?";
+    //değişken olan yerlere ? işareti koyuyoruz.
+    private String updateQuery02 = "update admin_notifications set is_read=1 where id=3";
+    private String preparedQuery02 = "update admin_notifications set is_read= ? where id= ?";
+    private String insertPreparedQuery01 = "insert into admin_password_resets (id, email,token, status, created_at) values (?,?,?,?,?)";
+    private String insertPreparedQuery02 = "insert into device_tokens (id, user_id, is_app, token, created_at, updated_at) values (?, ?, ?, ?,?,?)";
+    private String deletePreparedQuery01 = "delete from extensions where id=?";
+    private String deleteControlQuery = "select * from extensions where id = ?";
 
 
 
@@ -32,4 +34,33 @@ public class QueryManage {
     }
 
 
+    public String getPreparedQuery01() {
+        return preparedQuery01;
+    }
+
+    public String getUpdateQuery02() {
+        return updateQuery02;
+    }
+
+    public String getPreparedQuery02() {
+        return preparedQuery02;
+    }
+
+    public String getInsertPreparedQuery01() {
+        return insertPreparedQuery01;
+    }
+
+
+    public String getInsertPreparedQuery02() {
+        return insertPreparedQuery02;
+    }
+
+
+    public String getDeletePreparedQuery01() {
+        return deletePreparedQuery01;
+    }
+
+    public String getDeleteControlQuery() {
+        return deleteControlQuery;
+    }
 }
--- src/test/java/stepDefinitions/stepDefinition.java
+++ src/test/java/stepDefinitions/stepDefinition.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class stepDefinition {
 
@@ -26,6 +27,8 @@ public class stepDefinition {
     QueryManage queryManage = new QueryManage();
 
     int intResult;
+    int silinecekID;
+
 
     @Given("Database connection kurar")
     public void database_connection_kurar() throws SQLException {
@@ -156,6 +159,174 @@ public class stepDefinition {
     }
 
 
+    //UpdateQuery02
+
+    @Given("Updatequery02 hazirlanir ve calistirilir.")
+    public void updatequery02_hazirlanir_ve_calistirilir() {
+
+    Query = queryManage.getUpdateQuery02();
+
+    intResult = JDBCReusableMethods.updateQuery(Query);
+
+    }
+    @Given("Updatequery02 sonuclari islenir.")
+    public void updatequery02_sonuclari_islenir() {
+
+        Assert.assertEquals(1,intResult);
+
+    }
+
+
+
+
+    // ***** Prapered Statement Query Executions ********************
+    // @updatePraperedQuery01
+
+
+    @Given("UpdatePraperedQuery01 hazirlanir ve calistirilir.")
+    public void update_prapered_query01_hazirlanir_ve_calistirilir() throws SQLException {
+
+        Query = queryManage.getPreparedQuery01();
+
+        preparedStatement = JDBCReusableMethods.getConnection().prepareStatement(Query);
+
+        // "users" tablosunda sondan bir önceki harfi r olan
+        // "usernamelerin" "mobile" numarasını update ediniz
+
+        // update users set mobile = ? where username like ?
+
+        preparedStatement.setString(1,"85462");
+        preparedStatement.setString(2,"%r_");
+
+        intResult =preparedStatement.executeUpdate();
+    }
+    @Given("UpdatePraperedQuery01 sonuclari islenir.")
+    public void update_prapered_query01_sonuclari_islenir() {
+
+        assertEquals(1,intResult);
+
+    }
+
+    //@updatePreparedQuery02
+
+    @Given("preparedUpdatequery02 hazirlanir ve calistirilir.")
+    public void prepared_updatequery02_hazirlanir_ve_calistirilir() throws SQLException {
+
+        Query = queryManage.getPreparedQuery02();
+
+        preparedStatement = JDBCReusableMethods.getConnection().prepareStatement(Query);
+
+
+        //update admin_notifications set is_read = ? where id = ?
+        preparedStatement.setInt(1,1);
+        preparedStatement.setInt(2,4);
+
+        intResult =preparedStatement.executeUpdate();
+
+    }
+    @Given("preparedUpdatequery02 sonuclari islenir.")
+    public void prepared_updatequery02_sonuclari_islenir() {
+
+        Assert.assertEquals(1,intResult);
+    }
+
+
+
+    // insertPreparedQuery01
+
+    @Given("insertPreparedQuery hazirlanir ve calistirilir.")
+    public void insert_prepared_query_hazirlanir_ve_calistirilir() throws SQLException {
+
+        Query = queryManage.getInsertPreparedQuery01();
+
+        preparedStatement = JDBCReusableMethods.getConnection().prepareStatement(Query);
+
+        //insert into admin_password_resets (id, email,token, status, created_at) values
+        // 2210,'info@loantechexper.com','234267',1,'2024-02-10-00:00:00'
+
+
+        preparedStatement.setInt(1, 2211);
+        preparedStatement.setString(2,"test141@gmail.com");
+        preparedStatement.setString(3,"234267");
+        preparedStatement.setInt(4,1);
+        preparedStatement.setString(5,"2024-02-10-00:00:00");
+
+        intResult =preparedStatement.executeUpdate();
+
+    }
+    @Given("insertPreparedQuery sonuclari dogrulanir.")
+    public void insert_prepared_query_sonuclari_dogrulanir() {
+
+        assertEquals(1,intResult);
+    }
+
+
+
+    // insertPreparedQuery02
+
+    @Given("insertPreparedQuery02 hazirlanir ve calistirilir.")
+    public void insert_prepared_query02_hazirlanir_ve_calistirilir() throws SQLException {
+
+        Query = queryManage.getInsertPreparedQuery02();
+
+        preparedStatement = JDBCReusableMethods.getConnection().prepareStatement(Query);
+
+        //insert into device_tokens (id, user_id, is_app, token, created_at, updated_at ) values
+        //    (1, 0,3,'token141','2024-02-10-00:00:00','2024-02-10-00:00:00');
+
+        preparedStatement.setInt(1, 2);
+        preparedStatement.setInt(2,0);
+        preparedStatement.setInt(3,3);
+        preparedStatement.setString(4,"token141");
+        preparedStatement.setString(5,"2024-02-10-00:00:00");
+        preparedStatement.setString(6,"2024-02-10-00:00:00");
+
+
+        intResult =preparedStatement.executeUpdate();
+
+
+    }
+    @Given("insertPreparedQuery02 sonuclari dogrulanir.")
+    public void insert_prepared_query02_sonuclari_dogrulanir() {
+
+        assertEquals(1,intResult);
+    }
+
+
+
+    //deletePreparedQuery01
+
+    @Given("deleteQuery01 hazirlanir ve calistirilir.")
+    public void delete_query01_hazirlanir_ve_calistirilir() throws SQLException {
+
+        Query = queryManage.getDeletePreparedQuery01();
+        silinecekID = 3;
+        preparedStatement = JDBCReusableMethods.getConnection().prepareStatement(Query);
+
+
+        preparedStatement.setInt(1,silinecekID);
+
+
+        intResult =preparedStatement.executeUpdate();
+
+    }
+    @Given("deleteQuery01 sonuclari dogrulanir.")
+    public void delete_query01_sonuclari_dogrulanir() throws SQLException {
+
+        assertEquals(1, intResult);  // standart dogrulama
+
+        String controlQuery = queryManage.getDeleteControlQuery();
+
+        preparedStatement = JDBCReusableMethods.getConnection().prepareStatement(controlQuery);
+        preparedStatement.setInt(1,silinecekID);
+
+        resultSet = preparedStatement.executeQuery();
+
+        assertFalse(resultSet.next());  // control dogrulamasi
+
+
+
+    }
 
 
 
